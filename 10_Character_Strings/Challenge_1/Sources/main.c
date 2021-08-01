6e81708a998989deecf85e532138387234e48037
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2021-08-01 01:37:42 -0400

    Updated module 10 examples.

---
+++ 10_Character_Strings/Challenge_1/Sources/main.c
@@ -0,0 +1,16 @@
+/*
+ * Author: plasticuproject
+ * Purpose: Module 10 Challenge 1, Understanding
+ *          char arrays.
+ * Date: July 14, 2021
+*/
+
+
+#include <stdio.h>
+
+
+// main function
+int main(void) {
+
+    return 0;
+}
--- 10_Character_Strings/Example/Sources/main.c
+++ 10_Character_Strings/Example/Sources/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>               // islower(), isupper(), isalpha(), isdigit()
 
 #include "s_new_from_stdin.c"    // s_get_ahead()
 
@@ -24,9 +25,9 @@ int main(void) {
 
     // Constant Strings
     //  * Make sure the variable cannot be changed
-    // #define MONTHS 12  // preprocessor directive, subbed at/before compile time
-    // const int MONTHS = 12;  // symbolic constant (C90)
-    // ENUMS are a constant as well
+    //   *  #define MONTHS 12  // preprocessor directive, subbed at/before compile time
+    //   *  const int MONTHS = 12;  // symbolic constant (C90)
+    //   * ENUMS are a constant as well
 
     // Common String Functions:       // string.h
 
@@ -65,14 +66,96 @@ int main(void) {
     printf("%d\n", strcmp("apple", "apples"));
 
     if (strncmp("astronomy", "astro", 5) == 0) { printf("ASTRO\n"); }
-    char *userInput;
-    userInput = "\0";
-    while (strncmp(userInput, "quit", 4)) {    // compare first n values
-        if (strcmp(userInput, "\0") == 1) { free(userInput); }
+
+    char *userInput = "\0";
+    while (strncmp(userInput, "quit", 4)) {
+        if (strcmp(userInput, "\0") != 0) { free(userInput); userInput = NULL; }
         printf("Type 'quit' to stop: ");
         userInput = s_get_ahead();
     }
     free(userInput);
+    userInput = NULL;
+
+    // Searching, Tokenizing, and Analyzing
+      // Searching
+
+      // * strchr(), search string for character, returns NULL if not found
+    char *thing = NULL;
+    const char searchThis[] = "Search for A character";
+    const char searchChar = 'A';
+
+    thing = strchr(searchThis, searchChar);
+    if (thing != NULL) {
+        printf("'%c' is found in '%s'\n", thing[0], searchThis);
+        printf("The string from the match is '%s'\n", thing);
+    }
+    else { printf("'%c' Not found in '%s'\n", searchChar, searchThis); }
+
+      // * strstr(), like strchr(), but searches for an entire string/substring
+    char searchSubstring[] = "for";
+    thing = NULL;
+    thing = strstr(searchThis, searchSubstring);
+    if (thing != NULL) { printf("'%s' found in '%s'\n", searchSubstring, searchThis); }
+
+      // Tokenizing
+      // * strtok(), suppy string to be tokenized, and a string with all delimiters
+    char name[] = "Hello how are you--my name is-plastic";
+    const char s[2] = "-";
+    char *token = NULL;
+
+    token = strtok(name, s);
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, s);
+    }
+
+      // Analyzing; There are a lot more than listed here
+    char up = 'A';
+    char low = 'a';
+    char alp = 'B';
+    char dig = '1';
+
+      // * islower()
+    printf("islower('A') = %d\n", islower(up));
+    printf("islower('a') = %d\n", islower(low));
+    printf("islower('B') = %d\n", islower(alp));
+    printf("islower('1') = %d\n", islower(dig));
+      // * isupper()
+    printf("\nisupper('A') = %d\n", isupper(up));
+    printf("isupper('a') = %d\n", isupper(low));
+    printf("isupper('B') = %d\n", isupper(alp));
+    printf("isupper('1') = %d\n", isupper(dig));
+      // * isalpha()
+    printf("\nisalpha('A') = %d\n", isalpha(up));
+    printf("isalpha('a') = %d\n", isalpha(low));
+    printf("isalpha('B') = %d\n", isalpha(alp));
+    printf("isalpha('1') = %d\n", isalpha(dig));
+      // * isdigit()
+    printf("\nisdigit('A') = %d\n", isdigit(up));
+    printf("isdigit('a') = %d\n", isdigit(low));
+    printf("isdigit('B') = %d\n", isdigit(alp));
+    printf("isdigit('1') = %d\n", isdigit(dig));
+
+    // Converting Strings
+    // * toupper()
+    char lowString[] = "here is some text";
+    char upString[strlen(lowString) + 1];
+    for (int i = 0; (upString[i] = ((char)toupper(lowString[i]))) != '\0'; i++);
+    printf("\n%s\n", upString);
+
+    // * tolower()
+    for (int i = 0; (lowString[i] = ((char)tolower(upString[i]))) != '\0'; i++);
+    printf("\n%s\n", lowString);
+
+    for (int i = 0; i < strlen(lowString); i++) {
+        if (i == 0) { printf("\n%c", (char)toupper(lowString[i])); }
+        else { printf("%c", lowString[i]); }
+    }
+    printf(".\n");
+
+    // * atof(), atoi(), atol(), atoll(), (stdlib?)
+    // * strtod(), strtof(), strtold(), (C99??)
+
 
 
     return 0;
